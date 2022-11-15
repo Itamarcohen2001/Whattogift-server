458ceccb7e3d75fa0b9a9f5b59602f143aa93723
Itamarcohen2001 <Itamarcohen2001@gmail.com>
2022-11-15 09:58:02 +0200

    1511/1

--- controllers/company.js
+++ controllers/company.js
@@ -1,46 +1,37 @@
 import express from 'express';
 const router = express.Router();
+import Auth from './auth.js';
+import Company from '../models/company.js';
+import mongoose from 'mongoose';
 
-router.post('/create_company', async (req, res) => {
-    //check if compnay exist under the associate id!!!!!!
-    // Get company register data
-    const id = mongoose.Types.ObjectId();
-    // Check if company exist under the associate id
-    const { associatedId, companyName, contact, logo, bio } = req.body;
+router.post('/create_company',Auth, async (req, res) => {
 
-    // Check if company exists
-    Company.findOne({ companyName })
-        .then(async company => {
-            // if company with the given name exists
-            if (company) {
-                return res.status(200).json({
-                    status: false,
-                    message: 'Company name is not available.'
-                });
-            } else {
-                const _company = new Company({
-                    _id: id,
-                    associatedId: associatedId,
-                    companyName: companyName,
-                    contact: contact,
-                    logo: logo,
-                    bio: bio
-                })
-                _company.save()
-
-                    .then(company_created => {
-                        return res.status(200).json({
-                            status: true,
-                            message: "Company Created."
-                        });
-                    })
-                    .catch(error => {
-                        return res.status(500).json({
-                            status: false,
-                            message: error.message
-                        });
-                    })
-            }
+    const user =req.user;
+    const {companyName, contact} = req.body;
+    const company = await Company.find({associateId:user._id});
+    if(company.length > 0){
+        // Check if company exist under the associate id
+        return res.status(200).json({
+            status: false,
+            message: 'Company exist.'
+        });
+    }else{
+        const id = mongoose.Types.ObjectId();
+        const {companyName, contact} = req.body;
+        //create company
+        const _company = new Company({
+            _id: id,
+            associatedId: user._id,
+            companyName: companyName,
+            contact: contact,
+            bio: ''
+        })
+        _company.save()
+        .then(company_created => {
+            return res.status(200).json({
+                status: true,
+                message: company_created
+            });
         })
         .catch(error => {
             return res.status(500).json({
@@ -48,18 +39,21 @@ router.post('/create_company', async (req, res) => {
                 message: error.message
             });
         })
+    }
+       
 })
 
 
-router.post('/update_company', async (req, res) => {
+router.post('/update_company',Auth, async (req, res) => {
+    const user =req.user;
     //check if compnay exist under the associate id!!!!!!
-    const { associatedId, companyName, contact, logo, bio } = req.body;
+    const { associateId, companyName, contact, logo, bio } = req.body;
 
     // Check if company exists
-    Company.findOne({ companyName })
+    Company.findById(associateId)
         .then(async company => {
             if (company) {
-                company.associatedId = associatedId;
+                
                 company.companyName = companyName;
                 company.contact = contact;
                 company.logo = logo;
@@ -68,7 +62,7 @@ router.post('/update_company', async (req, res) => {
 
                 // update the details
                 return res.status(200).json({
-                    status: false,
+                    status: true,
                     message: 'Company details got updated.'
                 });
             } else {
