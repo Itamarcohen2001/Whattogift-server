b03316081a0279ea7616dfa06e3524368e1a8520
Itamarcohen2001 <Itamarcohen2001@gmail.com>
2022-11-30 10:16:26 +0200

    29/11/2022-2

--- controllers/Product.js
+++ controllers/Product.js
@@ -6,7 +6,25 @@ import Category from '../models/category.js';
 import Brand from '../models/brand.js';
 import Product from '../models/product.js';
 
-router.get('/get_all_brands', Auth, async(req,res) => {
+
+
+/**
+ * @swagger
+ * /api/product/get_all_brands:
+ *  get:
+ *   summary: Return a list of all brands
+ *   tags: [Products]
+ *   responses:
+ *    200:
+ *     description: This is the list of all brands
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *    500:
+ *     description: Error was found
+ */
+router.get('/get_all_brands', async(req,res) => {
     Brand.find()
     .then(brands => {
         return res.status(200).json({
@@ -15,7 +33,71 @@ router.get('/get_all_brands', Auth, async(req,res) => {
     })
     .catch(error => { return res.status(500).json({message: error.message})})
 })
-router.post('/create_new_brand', Auth, async(req,res) => {
+/** 
+ * @swagger
+ * /api/product/get_brand_by_id/{id}:
+ *  get:
+ *   summary: Get brand name by id
+ *   tags: [Products]
+ *   parameters:
+ *    - in: path 
+ *      name: id
+ *      schema: 
+ *       type: string
+ *      required: true
+ *   responses:
+ *    200:
+ *     description: Brand success
+ *    500:
+ *     description: Error was found
+*/
+router.get('/get_brand_by_id/:id', async(req,res) => {
+    Brand.findById(req.params.id)
+    .then(brand => {
+        return res.status(200).json({
+            message: brand
+        })
+    })
+    .catch(error => { return res.status(500).json({message: error.message})})
+})
+
+
+
+/**
+ * @swagger
+ * definitions:
+ *  Brand:
+ *   type: object
+ *   properties: 
+ *    brandName: 
+ *     type: string 
+ *     description: The name of the brand
+ *     example: Nike
+ *    brandLogo:
+ *     type: string 
+ *     description: Copy and paste image url
+ *     example: nike_logo.png 
+ */
+/**
+ * @swagger
+ * /api/product/create_new_brand:
+ *  post:
+ *   summary: Create new brand
+ *   descreption: Use this endpoint to create a new brand
+ *   tags: [Products]
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/definitions/Brand'
+ *   responses:
+ *    200:
+ *     description: Brand created
+ *    500:
+ *     description: Failure in Created brand
+ *      
+ */
+router.post('/create_new_brand', async(req,res) => {
     const {brandName,brandLogo} = req.body;
     const id = mongoose.Types.ObjectId();
     const _brand = new Brand({
@@ -29,7 +111,23 @@ router.post('/create_new_brand', Auth, async(req,res) => {
     })
     .catch(error => {return res.status(500).json({message: error.message})})
 })
-router.get('/get_all_categories', Auth, async(req,res) => {
+/**
+ * @swagger
+ * /api/product/get_all_categories:
+ *  get:
+ *   summary: Return a list of all categories
+ *   tags: [Products]
+ *   responses:
+ *    200:
+ *     description: This is the list of all categories
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *    500:
+ *     description: Error was found
+ */
+router.get('/get_all_categories',  async(req,res) => {
     Category.find()
     .then(categories => {
         return res.status(200).json({
@@ -51,7 +149,26 @@ router.post('/create_new_category', Auth, async(req,res) => {
     })
     .catch(error => {return res.status(500).json({message: error.message})})
 })
-router.get('/get_all_products', Auth, async(req,res) => {
+
+
+
+/**
+ * @swagger
+ * /api/product/get_all_products:
+ *  get:
+ *   summary: Return a list of all products
+ *   tags: [Products]
+ *   responses:
+ *    200:
+ *     description: This is the list of all products
+ *     content:
+ *      application/json:
+ *       schema:
+ *        type: array
+ *    500:
+ *     description: Error was found
+ */
+router.get('/get_all_products',  async(req,res) => {
     Product.find()
     .then(products => {
         return res.status(200).json({
@@ -91,9 +208,6 @@ router.post('/create_new_product', Auth, async(req,res) => {
         })
     })
 })
-
-
-
 
 
 
